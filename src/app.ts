71e32209bde571b701da431ee7ba983c3b8494c9
OlgaLiubar <olgaliubar@yandex.ru>
2021-10-21 16:34:52 +0300

    feat: updateListeners method

--- src/app.ts
+++ src/app.ts
@@ -63,6 +63,18 @@ class ProjectState extends State<Project> {
     const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
 
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find(prj => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -223,7 +235,10 @@ implements DragTarget {
 
   @Autobind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData('text/plain'));
+    const prjId = event.dataTransfer!.getData('text/plain');
+    projectState.moveProject(
+      prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished
+      );
   }
 
   @Autobind
